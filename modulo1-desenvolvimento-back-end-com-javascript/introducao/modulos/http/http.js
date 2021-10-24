10c0e78fa350375fa11e8613ab44fe53110601f6
vitorAlves7 <vitor.alves07750@outlook.com>
2021-10-24 11:52:00 -0300

    Adicionando códido da aula sobre o módulo http nativo do node

---
+++ modulo1-desenvolvimento-back-end-com-javascript/introducao/modulos/http/http.js
@@ -0,0 +1,12 @@
+import http from "http";
+
+http.createServer((req,res) => {
+    if((req.method === "GET") && (req.url === "/teste")){
+        res.write("GET /teste executado com sucesso");
+    } else {
+        res.write("Hello World!");
+    }
+    res.statusCode = 200;
+    res.end();
+}).listen(8080);
+
