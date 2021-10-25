c9cf69f4c497ca4f37334827ef58c50d7027a9cd
vitorAlves7 <vitor.alves07750@outlook.com>
2021-10-25 11:07:38 -0300

    Adicionando codigo da aula dee Rotas a respeitos dos operadores * () e Regex, além de falar sobre passagem de parametros pela url ou pelo body

--- modulo1-desenvolvimento-back-end-com-javascript/express/rotas/index.js
+++ modulo1-desenvolvimento-back-end-com-javascript/express/rotas/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 const app = express();
+app.use(express.json())
 
 app.all("/testAll",(req,res) =>{
     res.send(req.method);
@@ -13,6 +14,27 @@ app.get("/teste?",(_req,res) =>{
 app.get("/buzz+",(_req,res) =>{
     res.send("/buzz+");
 });
+
+app.get("/one*Blue",(req,res) => {
+    res.send(req.path);
+});
+
+app.post("/test(ing)+", (req,res) => {
+    console.log(req.body) ;
+    res.send("/test(ing)+");
+});
+
+app.get(/.*Red$/, (req,res) => {
+    res.send("/.*Red$/");
+});
+
+//parametros na rota
+
+app.get("/testParam/:id/:a?",(req,res) => {
+    res.send(req.params.id + " " + req.params.a);
+});
+
+
 
 app.listen(3000, () => {
     console.log("API Started!");
