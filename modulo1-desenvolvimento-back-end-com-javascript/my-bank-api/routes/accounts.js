1a8e709716f5a487acc013c7c27d7cb6ee7547f2
vitorAlves7 <vitor.alves07750@outlook.com>
2021-10-27 12:17:23 -0300

    Adicionando código do metodo get por id que seleciona uma account especifica

--- modulo1-desenvolvimento-back-end-com-javascript/my-bank-api/routes/accounts.js
+++ modulo1-desenvolvimento-back-end-com-javascript/my-bank-api/routes/accounts.js
@@ -7,19 +7,13 @@ const { readFile, writeFile } = fs;
 const router = express.Router();
 
 router.post("/", async (req,res) => {
-    
-    try{
-        
+    try{    
         let account = req.body;
         const data =  JSON.parse(await readFile(global.fileName));
-    
         account = {id: data.nextId++,...account};
         data.accounts.push(account);
-
-        await writeFile(global.fileName, JSON.stringify(data,null,2));
-        
+        await writeFile(global.fileName, JSON.stringify(data,null,2));        
         res.send(account);
-
     } catch(err){
         res.status(400).send({ error: err.message });
     }
@@ -38,6 +32,17 @@ router.get("/", async (req,res) => {
     }
 });
 
+router.get("/:id", async (req, res) =>{
+    try{
+        const data = JSON.parse(await readFile(global.fileName));
+        const account =data.accounts.find(
+            account => account.id === parseInt(req.params.id));
+        res.send(account);
+        
+    } catch(err){
+        res.status(400).send({ error: err.message });
+    }
+});
 
 
 export default router;
