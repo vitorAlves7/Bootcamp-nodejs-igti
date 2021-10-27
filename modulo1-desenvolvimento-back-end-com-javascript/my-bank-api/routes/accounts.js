6f74400ecf8af600e702123490d8c8d65dbccf18
vitorAlves7 <vitor.alves07750@outlook.com>
2021-10-27 10:50:45 -0300

    Adicionando codigos da criação da rota account e do metodo post da mesma

--- modulo1-desenvolvimento-back-end-com-javascript/my-bank-api/routes/accounts.js
+++ modulo1-desenvolvimento-back-end-com-javascript/my-bank-api/routes/accounts.js
@@ -1,6 +1,32 @@
 import express from "express";
+import { promises as fs } from "fs";
+
+
+const { readFile, writeFile } = fs;
+
 const router = express.Router();
 
+router.post("/", async (req,res) => {
+    
+    try{
+        
+        let account = req.body;
+        const data =  JSON.parse(await readFile("accounts.json"));
+    
+        account = {id: data.nextId++,...account};
+        data.accounts.push(account);
 
-router.post
+        await writeFile("accounts.json", JSON.stringify(data,null,2));
+        
+        res.send(account);
+
+    } catch(err){
+        res.status(400).send({ error: err.message });
+    }
+    
+
+
+});
+
+
 export default router;
